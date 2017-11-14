bf5ae63f3004ae9638da70c1b86a89bfee82bc21
cbulsara <bulsaracyrus@gmail.com>
2017-11-14 15:01:02 -0800

    First commit

---
+++ get_sets.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python27
+
+import requests
+import argparse
+
+url = 'https://10.120.40.130/api/'
+module = 'reference_data/sets'
+api_token = ''
+api_version = '8.0'
+
+def initArgParser():                    
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-k", "--key", help="Provide an API key on the command line.")
+    parser.add_argument("-kF", "--keyfile", help="Provide path to a text file that contains the API Key")
+    return parser.parse_args()
+
+def readKeyFile(p):
+    f = open(p, "r")
+    return f.readline()
+
+def main():
+    if args.key:
+        api_token = args.key
+    elif args.keyfile:
+        api_token = readKeyFile(args.keyfile)
+    else:
+        print "Please provide an API key."
+
+    r = requests.get(url + module, verify=False, headers={'SEC':api_token, 'Version':api_version})
+
+if __name__ == "__main__":
+    main()
